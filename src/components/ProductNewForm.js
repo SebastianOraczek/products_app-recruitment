d68e79da0d3152d9cbdc0ba864cd705b38f17fbf
Sebastian Oraczek <oraczek.sebastian@gmail.com>
2021-10-14 14:59:23 +0200

    Products app

--- src/components/ProductNewForm.js
+++ src/components/ProductNewForm.js
@@ -70,16 +70,8 @@ function NewProduct({ history }) {
                 tax_id: oneTax
             };
 
-            await axios.post(url, body, {
-                headers: {
-                    Authorization: "fd9ba9e1-0788-4e8f-ac46-a43df43e205e",
-                    Accept: "application/json",
-                    // "Access-Control-Allow-Headers": "X-Requested-With",
-                    // "Access-Control-Allow-Origin": "*",
-                    // "Access-Control-Allow-Methods": 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-                    // "Content-Encoding": "gzip"
-                },
-            });
+            await axios.post(url, body, { headers, });
+
         } catch (err) {
             console.log(err);
         };
--- src/utils/headers.js
+++ src/utils/headers.js
@@ -2,8 +2,12 @@ const headers = {
     Authorization: "fd9ba9e1-0788-4e8f-ac46-a43df43e205e",
     Accept: "application/json",
     "Content-Type": "application/json",
-    // "Access-Control-Allow-Headers": "X-Requested-With",
     "Access-Control-Allow-Origin": "*",
+
+    // "Access-Control-Allow-Headers": "X-Requested-With",
+    // "Access-Control-Allow-Origin": "*",
+    // "Access-Control-Allow-Methods": 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    // "Content-Encoding": "gzip"
 }
 
 export default headers;
